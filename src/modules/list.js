9cdb9316b92ae774eacac17f4ac35bec01ce3a7b
George <georgie.george7@gmail.com>
2023-07-29 17:12:45 +0400

    Remove unnecessary code

--- src/modules/list.js
+++ src/modules/list.js
@@ -1,44 +1,44 @@
-const result = [
-  {
-    user: 'Name: ',
-    score: 100,
-  },
-  {
-    user: 'Name: ',
-    score: 20,
-  },
-  {
-    user: 'Name: ',
-    score: 50,
-  },
-  {
-    user: 'Name: ',
-    score: 78,
-  },
-  {
-    user: 'Name: ',
-    score: 125,
-  },
-  {
-    user: 'Name: ',
-    score: 77,
-  },
-  {
-    user: 'Name: ',
-    score: 42,
-  },
-];
+// const result = [
+//   {
+//     user: 'Name: ',
+//     score: 100,
+//   },
+//   {
+//     user: 'Name: ',
+//     score: 20,
+//   },
+//   {
+//     user: 'Name: ',
+//     score: 50,
+//   },
+//   {
+//     user: 'Name: ',
+//     score: 78,
+//   },
+//   {
+//     user: 'Name: ',
+//     score: 125,
+//   },
+//   {
+//     user: 'Name: ',
+//     score: 77,
+//   },
+//   {
+//     user: 'Name: ',
+//     score: 42,
+//   },
+// ];
 
-const scoreList = () => {
-  const scoreUl = document.createElement('ul');
-  scoreUl.className = 'scorelist';
-  result.forEach((data) => {
-    scoreUl.innerHTML += `
-      <li>${data.user} ${data.score}</li>
-    `;
-  });
-  const ul = document.querySelector('.scores');
-  ul.appendChild(scoreUl);
-};
+// const scoreList = () => {
+//   const scoreUl = document.createElement('ul');
+//   scoreUl.className = 'scorelist';
+//   result.forEach((data) => {
+//     scoreUl.innerHTML += `
+//       <li>${data.user} ${data.score}</li>
+//     `;
+//   });
+//   const ul = document.querySelector('.scores');
+//   ul.appendChild(scoreUl);
+// };
 
-export default scoreList;
+// export default scoreList;
