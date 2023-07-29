24150dec5976b741d45186109b913a118471dd1a
George <georgie.george7@gmail.com>
2023-07-29 17:13:11 +0400

    Render page and initalize

--- src/index.js
+++ src/index.js
@@ -1,4 +1,15 @@
 import './index.css';
-import scoreList from './modules/list.js';
+import {
+  scoreList, refreshButtonListener, submitButtonListener,
+} from './modules/leaderboard.js';
 
-scoreList();
+const init = () => {
+  // Attach event listeners
+  refreshButtonListener();
+  submitButtonListener();
+
+  // Initial call to load scores when the page is first loaded
+  scoreList();
+};
+
+init();
